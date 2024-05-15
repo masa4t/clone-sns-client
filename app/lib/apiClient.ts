770299b12a6c14c2b53104f4d832f269254a45f0
masa4t <godo034t@gmail.com>
2024-05-15 19:32:54 +0900

    base URL

--- app/lib/apiClient.ts
+++ app/lib/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+
   headers: {
     "Contet-Type": "application/json",
   },
